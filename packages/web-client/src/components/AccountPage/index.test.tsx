5f72f0d61f16f381a0c16f006526d7dac37f53e6
David Christie <davidchristie.nz@gmail.com>
2018-08-24 17:15:55 +1200

    Add AccountPage unit tests

---
+++ packages/web-client/src/components/AccountPage/index.test.tsx
@@ -0,0 +1,11 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+
+import AccountPage from './index'
+
+describe('AccountPage component', () => {
+  it('matches snapshot', () => {
+    const wrapper = shallow(<AccountPage />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
