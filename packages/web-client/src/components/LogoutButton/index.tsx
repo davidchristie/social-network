64c4803013f7b6890937c391cd3d38fb728864fd
David Christie <davidchristie.nz@gmail.com>
2018-08-25 16:44:45 +1200

    Add LogoutButton unit tests

--- packages/web-client/src/components/LogoutButton/index.tsx
+++ packages/web-client/src/components/LogoutButton/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 import Button from "../Button";
+import { AUTHENTICATION_TOKEN } from '../../constants'
 
 const handleClick = () => {
-  window.localStorage.clear();
+  window.localStorage.removeItem(AUTHENTICATION_TOKEN);
   window.location.reload();
 };
 
-export default class LayoutButton extends React.Component {
+export default class LogoutButton extends React.Component {
   public render () {
     return (
       <Button onClick={handleClick}>
