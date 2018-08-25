781220ca3aff44a7d4811ccf7bdf5b9cf3e4e453
David Christie <davidchristie.nz@gmail.com>
2018-08-26 09:04:44 +1200

    Rename prisma to social-network

--- packages/infrastructure/scripts/connect-to-prisma.sh
+++ packages/infrastructure/scripts/connect-to-prisma.sh
@@ -1,2 +1,2 @@
-PRISMA_POD_NAME=$(kubectl get pods --namespace prisma | grep -o "prisma[^ ]*")
-kubectl port-forward --namespace prisma $PRISMA_POD_NAME 4467:4466
+PRISMA_POD_NAME=$(kubectl get pods --namespace social-network | grep -o "prisma[^ ]*")
+kubectl port-forward --namespace social-network $PRISMA_POD_NAME 4467:4466
--- packages/infrastructure/scripts/deploy.sh
+++ packages/infrastructure/scripts/deploy.sh
@@ -1,35 +1,9 @@
-echo "Apply prisma namespace"
-
 kubectl apply -f namespace.yml
-
-echo "Request MySQL database"
-
 kubectl apply -f database/pvc.yml
-
-echo "Apply MySQL deployment definition"
-
 kubectl apply -f database/deployment.yml
-
-echo "Apply MySQL service definition"
-
 kubectl apply -f database/service.yml
-
-echo "Deploy Prisma ConfigMap"
-
 kubectl apply -f prisma/configmap.yml
-
-echo "Apply Prisma deployment definition"
-
 kubectl apply -f prisma/deployment.yml
-
-echo "Apply Prisma service definition"
-
 kubectl apply -f prisma/service.yml
-
-echo "Apply public-api deployment definition"
-
 kubectl apply -f public-api/deployment.yml
-
-echo "Apply public-api service definition"
-
 kubectl apply -f public-api/service.yml
