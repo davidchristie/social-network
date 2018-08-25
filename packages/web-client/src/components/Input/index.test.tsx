1d07e99392fadab4d03cda7372226f8be753993f
David Christie <davidchristie.nz@gmail.com>
2018-08-25 15:42:16 +1200

    Add Input unit tests

---
+++ packages/web-client/src/components/Input/index.test.tsx
@@ -0,0 +1,36 @@
+import { shallow, ShallowWrapper } from 'enzyme'
+import React from 'react'
+
+import Input from './index'
+
+describe('Input component', () => {
+  let wrapper: ShallowWrapper
+
+  describe('with no props', () => {
+    beforeEach(() => {
+      wrapper = shallow(<Input />)
+    })
+
+    it('matches snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
+
+  describe('with custom class', () => {
+    const CUSTOM_CLASS = 'custom_class'
+
+    beforeEach(() => {
+      wrapper = shallow(
+        <Input className={CUSTOM_CLASS} />
+      )
+    })
+
+    it('has custom class', () => {
+      expect(wrapper.hasClass(CUSTOM_CLASS)).toBe(true)
+    })
+
+    it('matches snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
+})
