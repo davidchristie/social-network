d832b1344344658f42621903ff37818d2404f307
David Christie <davidchristie.nz@gmail.com>
2018-08-25 14:25:45 +1200

    Add Alert unit tests

---
+++ packages/web-client/src/components/Alert/index.test.tsx
@@ -0,0 +1,36 @@
+import { shallow, ShallowWrapper } from 'enzyme'
+import React from 'react'
+
+import Alert from './index'
+
+describe('Alert component', () => {
+  let wrapper: ShallowWrapper
+
+  describe('with no props', () => {
+    beforeEach(() => {
+      wrapper = shallow(<Alert />)
+    })
+
+    it('matches snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
+
+  describe('with custom class', () => {
+    const CUSTOM_CLASS = 'custom_class'
+
+    beforeEach(() => {
+      wrapper = shallow(
+        <Alert className={CUSTOM_CLASS} />
+      )
+    })
+
+    it('has custom class', () => {
+      expect(wrapper.hasClass(CUSTOM_CLASS)).toBe(true)
+    })
+
+    it('matches snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
+})
