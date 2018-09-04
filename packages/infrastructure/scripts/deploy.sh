da6de6023ea0e044e926c12a206aa232e20aff04
David Christie <davidchristie.nz@gmail.com>
2018-09-04 13:17:00 +0200

    Add public-api ingress

--- packages/infrastructure/scripts/deploy.sh
+++ packages/infrastructure/scripts/deploy.sh
@@ -1,1 +1,12 @@
-kubectl apply --filename definitions --recursive
+kubectl apply -f definitions/namespace.yml
+kubectl apply -f definitions/authentication/deployment.yml
+kubectl apply -f definitions/authentication/service.yml
+kubectl apply -f definitions/database/pvc.yml
+kubectl apply -f definitions/database/deployment.yml
+kubectl apply -f definitions/database/service.yml
+kubectl apply -f definitions/prisma/configmap.yml
+kubectl apply -f definitions/prisma/deployment.yml
+kubectl apply -f definitions/prisma/service.yml
+kubectl apply -f definitions/public-api/deployment.yml
+kubectl apply -f definitions/public-api/service.yml
+kubectl apply -f definitions/public-api/ingress.yml
