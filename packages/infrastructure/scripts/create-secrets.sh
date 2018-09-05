a7ae270fc989709efa0940abcffd0a283e6eb2b8
David Christie <davidchristie.nz@gmail.com>
2018-09-05 07:40:37 +0200

    Fix TLS certificate creation

--- packages/infrastructure/scripts/create-secrets.sh
+++ packages/infrastructure/scripts/create-secrets.sh
@@ -1,11 +1,22 @@
 # Load environment variables
 export $(grep -v '^#' ../../.env | xargs)
 
+# Generate certificate authority root
+openssl genrsa -des3 -out /tmp/ca.key 2048
+openssl req -x509 -new -nodes -key /tmp/ca.key -sha256 -days 1825 -out /tmp/ca.pem
+
+# Create CA-signed certificate
+openssl genrsa -out /tmp/www.social-network.com.key 2048
+openssl req -new -key /tmp/www.social-network.com.key -out /tmp/www.social-network.com.csr
+
 # Create TLS
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout /tmp/tls.key \
-  -out /tmp/tls.crt \
-  -subj "/CN=docker.local"
+openssl x509 -req -in /tmp/www.social-network.com.csr \
+  -CA /tmp/ca.pem \
+  -CAkey /tmp/ca.key \
+  -CAcreateserial \
+  -out /tmp/www.social-network.com.crt \
+  -days 1825 -sha256 \
+  -extfile www.social-network.com.ext
 
 # Create namespace
 scripts/create-namespace.sh
@@ -16,5 +27,5 @@ kubectl create secret generic authentication \
   --from-literal=jwt-secret=$JWT_SECRET || true
 kubectl create secret tls tls \
   --namespace=social-network \
-  --key /tmp/tls.key \
-  --cert /tmp/tls.crt || true
+  --key /tmp/www.social-network.com.key \
+  --cert /tmp/www.social-network.com.crt || true
