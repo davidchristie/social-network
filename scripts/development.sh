4ce8ebd304eee84e8bdcf4283d41f95851586c4f
David Christie <davidchristie.nz@gmail.com>
2018-10-04 23:02:21 +1300

    Run docker-compose build

--- scripts/development.sh
+++ scripts/development.sh
@@ -1,4 +1,8 @@
 docker-compose \
   -f docker-compose.yml \
   -f docker-compose.development.yml \
-  up --build --detach
+  build
+docker-compose \
+  -f docker-compose.yml \
+  -f docker-compose.development.yml \
+  up --detach
