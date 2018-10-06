a4451a7d57b6405925666f863fc70e240400974a
David Christie <davidchristie.nz@gmail.com>
2018-10-06 21:28:59 +1300

    Fix starting end-to-end container

--- scripts/test:end-to-end.sh
+++ scripts/test:end-to-end.sh
@@ -1,6 +1,10 @@
 docker-compose \
+  -f docker-compose.yml \
+  -f docker-compose.production.yml \
   -f docker-compose.testing.yml \
   build end-to-end
 docker-compose \
+  -f docker-compose.yml \
+  -f docker-compose.production.yml \
   -f docker-compose.testing.yml \
   up end-to-end
