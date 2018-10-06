b160feb42b3eadf4f543edec93982609abba3d64
David Christie <davidchristie.nz@gmail.com>
2018-10-07 00:31:12 +1300

    Run end-to-end container

--- scripts/production.sh
+++ scripts/production.sh
@@ -1,9 +1,8 @@
-$(docker-compose \
+docker-compose \
   -f docker-compose.yml \
   -f docker-compose.production.yml \
-  build --no-cache)
-$(docker-compose \
+  build --no-cache
+docker-compose \
   -f docker-compose.yml \
   -f docker-compose.production.yml \
-  up --detach)
-docker-compose logs end-to-end
+  up --detach
