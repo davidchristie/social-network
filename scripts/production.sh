63014f1d1d266ed9664fe29a9fde205c4370634a
David Christie <davidchristie.nz@gmail.com>
2018-10-06 22:01:19 +1300

    Run end-to-end tests during production build

--- scripts/production.sh
+++ scripts/production.sh
@@ -6,3 +6,7 @@ docker-compose \
   -f docker-compose.yml \
   -f docker-compose.production.yml \
   up --detach
+docker-compose \
+  -f docker-compose.yml \
+  -f docker-compose.production.yml \
+  logs end-to-end
