cce132c76ffe13a6ab06c14a4d56dd67c5ceb12c
David Christie <davidchristie.nz@gmail.com>
2018-10-10 08:16:01 +1300

    Refactor production script

--- scripts/production.sh
+++
@@ -1,8 +0,0 @@
-docker-compose \
-  -f docker-compose.yml \
-  -f docker-compose.production.yml \
-  build --no-cache
-docker-compose \
-  -f docker-compose.yml \
-  -f docker-compose.production.yml \
-  up --detach
