5ef95bf528d75d27f9a2bb260c6a8b26e5a15832
David Christie <davidchristie.nz@gmail.com>
2018-10-12 20:32:04 +1300

    Test dark Overlay matches snapshot

--- packages/web-client/src/components/Overlay/index.test.tsx
+++ packages/web-client/src/components/Overlay/index.test.tsx
@@ -4,8 +4,17 @@ import React from "react";
 import Overlay from "./index";
 
 describe("Overlay component", () => {
-  it("matches snapshot", () => {
-    const wrapper = shallow(<Overlay />);
-    expect(wrapper).toMatchSnapshot();
+  describe("with no props", () => {
+    it("matches snapshot", () => {
+      const wrapper = shallow(<Overlay />);
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe("with big prop", () => {
+    it("matches snapshot", () => {
+      const wrapper = shallow(<Overlay dark={true} />);
+      expect(wrapper).toMatchSnapshot();
+    });
   });
 });
