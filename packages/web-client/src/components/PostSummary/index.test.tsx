d723de16fcfea49fac3a15bd21f05a25bb0396d7
David Christie <davidchristie.nz@gmail.com>
2018-10-12 21:02:27 +1300

    Test PostSummary with avatar image

--- packages/web-client/src/components/PostSummary/index.test.tsx
+++ packages/web-client/src/components/PostSummary/index.test.tsx
@@ -7,7 +7,10 @@ import PostSummary from "./index";
 const post: PostSummaryData = {
   createdAt: "2018-10-12T07:48:03.948Z",
   createdBy: {
-    avatar: null,
+    avatar: {
+      id: "cjn5pxfvg001h0a42hqjvgo11",
+      url: "https:cdn.social-network.com/images/default-avatar.png",
+    },
     id: "cjn5pm4wm000r0a4239rvnn8i",
     name: "User 1",
   },
@@ -17,6 +20,20 @@ const post: PostSummaryData = {
 
 describe("PostSummary component", () => {
   describe("without avatar image", () => {
+    it("matches snapshot", () => {
+      const postWithoutAvatar: PostSummaryData = {
+        ...post,
+        createdBy: {
+          ...post.createdBy,
+          avatar: null,
+        },
+      };
+      const wrapper = shallow(<PostSummary post={postWithoutAvatar} />);
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe("with avatar image", () => {
     it("matches snapshot", () => {
       const wrapper = shallow(<PostSummary post={post} />);
       expect(wrapper).toMatchSnapshot();
