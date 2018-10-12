aad236c610cba9ca69a172f041f3f8af076ebd34
David Christie <davidchristie.nz@gmail.com>
2018-10-12 20:18:47 +1300

    Test ButtonLink with custom class

--- packages/web-client/src/components/ButtonLink/index.test.tsx
+++ packages/web-client/src/components/ButtonLink/index.test.tsx
@@ -17,4 +17,22 @@ describe("ButtonLink component", () => {
   it("matches snapshot", () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe("with custom class", () => {
+    const CUSTOM_CLASS = "custom_class";
+
+    beforeEach(() => {
+      wrapper = shallow(
+        <ButtonLink className={CUSTOM_CLASS} to={TO} />,
+      );
+    });
+
+    it("has custom class", () => {
+      expect(wrapper.hasClass(CUSTOM_CLASS)).toBe(true);
+    });
+
+    it("matches snapshot", () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
 });
