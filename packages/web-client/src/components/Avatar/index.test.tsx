b2640da0a934d661acc7771b66b3459bbcbf3157
David Christie <davidchristie.nz@gmail.com>
2018-10-12 20:03:05 +1300

    Test large Avatar matches snapshot

---
+++ packages/web-client/src/components/Avatar/index.test.tsx
@@ -0,0 +1,18 @@
+import { shallow, ShallowWrapper } from "enzyme";
+import React from "react";
+
+import Avatar from "./index";
+
+describe("Avatar component", () => {
+  let wrapper: ShallowWrapper;
+
+  describe("large", () => {
+    beforeEach(() => {
+      wrapper = shallow(<Avatar size="large" />);
+    });
+
+    it("matches snapshot", () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+});
