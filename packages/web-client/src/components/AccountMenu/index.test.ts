699a8a8d5052c9f52923505be1b5d78f14a64e70
David Christie <davidchristie.nz@gmail.com>
2018-10-16 20:41:08 +1300

    Test AccountMenu matches snapshot

---
+++ packages/web-client/src/components/AccountMenu/index.test.ts
@@ -0,0 +1,6 @@
+import describeWithNoProps from "../../testing/describeWithNoProps";
+import AccountMenu from "./index";
+
+describe("AccountMenu component", () => {
+  describeWithNoProps(AccountMenu);
+});
