d2a76d85c10d23f9f05f3e04f5ed185a661a505f
David Christie <davidchristie.nz@gmail.com>
2018-10-16 21:59:41 +1300

    Test without props

---
+++ packages/web-client/src/components/DeletePostButton/index.test.ts
@@ -0,0 +1,6 @@
+import describeWithNoProps from "../../testing/describeWithNoProps";
+import DeletePostButton from "./index";
+
+describe("DeletePostButton component", () => {
+  describeWithNoProps(DeletePostButton);
+});
