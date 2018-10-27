acefc2135f829ca74ef8145e6401028ec62d4524
David Christie <davidchristie.nz@gmail.com>
2018-10-27 17:17:07 +1300

    Refactor button to a function component

--- packages/libraries/design-system/src/components/Button/index.tsx
+++ packages/libraries/design-system/src/components/Button/index.tsx
@@ -4,16 +4,13 @@ import "./index.css";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> { }
 
-export default class Button extends React.Component<Props> {
-  public render() {
-    const { children, className, ...attributes } = this.props;
-    return (
-      <button
-        className={`Button${className ? " " + className : ""}`}
-        {...attributes}
-      >
-        {children}
-      </button>
-    );
-  }
-}
+const Button: React.SFC<Props> = ({ children, className, ...attributes }) => (
+  <button
+    className={`Button${className ? " " + className : ""}`}
+    {...attributes}
+  >
+    {children}
+  </button>
+);
+
+export default Button
