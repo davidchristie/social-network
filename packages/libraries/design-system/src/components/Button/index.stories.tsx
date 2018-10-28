cbc8e972858a52fb0ee04b7db4e31ca00c096ce1
David Christie <davidchristie.nz@gmail.com>
2018-10-28 17:32:28 +1300

    Write component stories in TypeScript

--- packages/libraries/design-system/src/components/Button/index.stories.tsx
+++ packages/libraries/design-system/src/components/Button/index.stories.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { withInfo } from "@storybook/addon-info";
 import { action } from "@storybook/addon-actions";
+import { withInfo } from "@storybook/addon-info";
+import { storiesOf } from "@storybook/react";
+import React from "react";
 
 import Button from ".";
 
