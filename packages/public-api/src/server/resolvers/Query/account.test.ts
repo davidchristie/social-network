76ee61b524e32e55dd1a22ff92cc6f9d7bf456cc
David Christie <davidchristie.nz@gmail.com>
2018-10-29 22:06:55 +1300

    Refactor tests

--- packages/public-api/src/server/resolvers/Query/account.test.ts
+++ packages/public-api/src/server/resolvers/Query/account.test.ts
@@ -5,55 +5,57 @@ const response = {} as Response;
 
 jest.mock("node-fetch", () => () => response);
 
+function itReturnsExpectedResult (
+  name: string,
+  authenticationData: any,
+  queryResult: any,
+) {
+  beforeEach(() => {
+    response.json = () => Promise.resolve(
+      authenticationData
+    );
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it(name, async () => {
+    const queryAccount = jest.fn();
+    queryAccount.mockResolvedValueOnce(queryResult);
+    const context = {
+      database: {
+        query: {
+          account: queryAccount,
+        },
+      },
+      request: {
+        get: jest.fn(),
+      },
+    } as unknown as Context;
+    expect(await accountQueryResolver(null, null, context, null))
+      .toBe(queryResult);
+  });
+}
+
 describe("account query resolver", () => {
   describe("with valid authentication", () => {
-    beforeEach(() => {
-      response.json = () => Promise.resolve({
+    itReturnsExpectedResult(
+      "returns account",
+      {
         id: "account_id",
-      });
-    });
-
-    it("returns account", async () => {
-      const result = {};
-      const queryAccount = jest.fn();
-      queryAccount.mockResolvedValueOnce(result);
-      const context = {
-        database: {
-          query: {
-            account: queryAccount,
-          },
-        },
-        request: {
-          get: jest.fn(),
-        },
-      } as unknown as Context;
-      expect(await accountQueryResolver(null, null, context, null))
-        .toBe(result);
-    });
+      },
+      {}
+    );
   });
 
   describe("without valid authentication", () => {
-    beforeEach(() => {
-      response.json = () => Promise.resolve({
+    itReturnsExpectedResult(
+      "returns null",
+      {
         id: null,
-      });
-    });
-
-    it("returns null", async () => {
-      const queryAccount = jest.fn();
-      queryAccount.mockResolvedValueOnce(null);
-      const context = {
-        database: {
-          query: {
-            account: queryAccount,
-          },
-        },
-        request: {
-          get: jest.fn(),
-        },
-      } as unknown as Context;
-      expect(await accountQueryResolver(null, null, context, null))
-        .toBe(null);
-    });
+      },
+      null
+    );
   });
 });
