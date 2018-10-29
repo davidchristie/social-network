b04c42b2cf633a884eb049737ddc78e1ea9533e7
David Christie <davidchristie.nz@gmail.com>
2018-10-29 21:08:41 +1300

    Test GraphQL server

--- packages/public-api/src/server/index.ts
+++ packages/public-api/src/server/index.ts
@@ -10,7 +10,7 @@ export default new GraphQLServer({
     account: await authentication.account(request),
     database: new Prisma({
       debug: true,
-      endpoint: process.env.PRISMA_ENDPOINT,
+      endpoint: process.env.PRISMA_ENDPOINT || "http://localhost:4467",
     }),
     request,
   }),
