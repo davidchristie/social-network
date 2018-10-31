d2bdfd6342130c80df1b56ececf85e174fbf63e2
David Christie <davidchristie.nz@gmail.com>
2018-11-01 05:20:26 +1300

    Add fallback to production API endpoint

--- packages/web-client/src/apollo/links/http.ts
+++ packages/web-client/src/apollo/links/http.ts
@@ -1,5 +1,7 @@
 import { HttpLink } from "apollo-link-http";
 
+const PRODUCTION_API_ENDPOINT = "https://api.social-network.davidchristie.io";
+
 export default new HttpLink({
-  uri: process.env.REACT_APP_API_ENDPOINT,
+  uri: process.env.REACT_APP_API_ENDPOINT || PRODUCTION_API_ENDPOINT,
 });
