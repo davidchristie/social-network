63179d55f6072b9d9ac24a5fc4ece4cbfec93d79
David Christie <davidchristie.nz@gmail.com>
2018-11-01 20:25:27 +1300

    Fix info addon deprecation warning

--- packages/libraries/design-system/.storybook/config.js
+++ packages/libraries/design-system/.storybook/config.js
@@ -1,6 +1,13 @@
-import { configure } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { addDecorator, configure } from '@storybook/react';
 
 const req = require.context('../src/components', true, /\.stories\.tsx?$/)
+
+addDecorator(
+  withInfo({
+    inline: true,
+  })
+);
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename))
