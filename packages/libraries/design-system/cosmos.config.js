8333e35ed5a0f2640dcd9e19ca22819e0e0df15c
David Christie <davidchristie.nz@gmail.com>
2018-11-03 09:02:37 +1300

    Update cosmos configuration to use react-scripts

--- packages/libraries/design-system/cosmos.config.js
+++ packages/libraries/design-system/cosmos.config.js
@@ -1,7 +1,7 @@
 // cosmos.config.js
 module.exports = {
   containerQuerySelector: '#root',
-  webpackConfigPath: 'react-scripts-ts/config/webpack.config.dev',
+  webpackConfigPath: 'react-scripts/config/webpack.config.dev',
   publicPath: 'public',
   // Optional: Add this when you start using proxies
   proxiesPath: 'src/cosmos.proxies'
