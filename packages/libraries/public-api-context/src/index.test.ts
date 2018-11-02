2048157f226c747876066cd835419320a64f343a
David Christie <davidchristie.nz@gmail.com>
2018-11-03 11:52:05 +1300

    Unit test public-api-context library

---
+++ packages/libraries/public-api-context/src/index.test.ts
@@ -0,0 +1,31 @@
+import { Request } from "express";
+import { ContextParameters } from "graphql-yoga/dist/types";
+
+import { createContext } from ".";
+
+jest.mock("node-fetch", () => () => Promise.resolve({
+  json () {
+    return Promise.resolve(null);
+  },
+}));
+
+const mockRequest = {
+  get (header: string) {
+    return null;
+  },
+} as Request;
+
+describe("createContext function", () => {
+  it("returns a callback function", () => {
+    const contextCallback = createContext();
+    expect(typeof contextCallback).toBe("function");
+  });
+
+  it("adds the request to context", async () => {
+    const contextCallback = createContext();
+    const context = await contextCallback({
+      request: mockRequest,
+    } as ContextParameters);
+    expect(context.request).toBe(mockRequest);
+  });
+});
