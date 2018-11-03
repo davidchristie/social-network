792cc8905ee6aa3c976fb5bbe852d5372e4bb2a2
David Christie <davidchristie.nz@gmail.com>
2018-11-03 19:15:37 +1300

    Fix authentication unit tests

--- packages/authentication/src/testing/mockPrisma.ts
+++ packages/authentication/src/testing/mockPrisma.ts
@@ -1,31 +1,27 @@
 jest.mock("../services/prisma", () => ({
-  mutation: {
-    createAccount: jest.fn(),
-    updateAccount: jest.fn(),
-  },
-  query: {
-    account: jest.fn(),
-  },
+  account: jest.fn(),
+  createAccount: jest.fn(),
+  updateAccount: jest.fn(),
 }));
 
 export function mockCreateAccountOnce (account: any) {
-  require("../services/prisma").mutation.createAccount
+  require("../services/prisma").createAccount
     .mockReturnValueOnce(account);
 }
 
 export function mockQueryAccountOnce (account: any) {
-  require("../services/prisma").query.account
+  require("../services/prisma").account
     .mockReturnValueOnce(account);
 }
 
 export function mockQueryAccountError (error: Error) {
-  require("../services/prisma").query.account
+  require("../services/prisma").account
     .mockImplementation(() => {
       throw error;
     });
 }
 
 export function mockUpdateAccountOnce (account: any) {
-  require("../services/prisma").mutation.updateAccount
+  require("../services/prisma").updateAccount
     .mockReturnValueOnce(account);
 }
