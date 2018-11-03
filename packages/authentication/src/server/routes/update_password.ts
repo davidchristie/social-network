ba17166aec82ea78dddb5da5f5185ef71d6a5ea0
David Christie <davidchristie.nz@gmail.com>
2018-11-04 01:46:02 +1300

    Refactor GET /update_password route

--- packages/authentication/src/server/routes/update_password.ts
+++ packages/authentication/src/server/routes/update_password.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { RequestHandler, Router } from "express";
 
 import prisma from "../../services/prisma";
 import getAccountById from "../../utilities/getAccountById";
@@ -7,30 +7,38 @@ import isPassword from "../../utilities/isPassword";
 import sendAccountNotFound from "../../utilities/sendAccountNotFound";
 import sendIncorrectPassword from "../../utilities/sendIncorrectPassword";
 
-const router = Router();
-router.post("/", async (request, response) => {
-  try {
-    const { accountId, currentPassword, newPassword } = request.body;
-    const account = await getAccountById(accountId);
-    if (!account) {
-      return sendAccountNotFound(response);
-    }
-    if (!await isPassword(currentPassword, account)) {
-      return sendIncorrectPassword(response);
-    }
-    await prisma.updateAccount({
-      data: {
-        password: await getHash(newPassword),
-      },
-      where: {
-        id: accountId,
-      },
-    });
-    response.sendStatus(200);
-  } catch (error) {
-    console.log(error.stack);
-    response.status(500).send(error.message);
+const requireExistingAccount: RequestHandler = async (request, response, next) => {
+  const { accountId } = request.body;
+  const account = await getAccountById(accountId);
+  if (!account) {
+    return sendAccountNotFound(response);
   }
-});
+  request.body.account = account;
+  return next();
+};
+
+const requireCorrectPassword: RequestHandler = async (request, response, next) => {
+  const { account, currentPassword } = request.body;
+  if (!await isPassword(currentPassword, account)) {
+    return sendIncorrectPassword(response);
+  }
+  return next();
+};
+
+const updateAccountPassword: RequestHandler = async (request, response) => {
+  const { account, newPassword } = request.body;
+  await prisma.updateAccount({
+    data: {
+      password: await getHash(newPassword),
+    },
+    where: {
+      id: account.id,
+    },
+  });
+  return response.sendStatus(200);
+};
+
+const router = Router();
+router.post("/", requireExistingAccount, requireCorrectPassword, updateAccountPassword);
 
 export default router;
