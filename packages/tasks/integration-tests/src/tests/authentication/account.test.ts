c47196ba57f6a471374f1ec1b2ad5a0e0fb516ab
David Christie <davidchristie.nz@gmail.com>
2018-11-03 15:51:04 +1300

    Test with invalid authentication header

--- packages/tasks/integration-tests/src/tests/authentication/account.test.ts
+++ packages/tasks/integration-tests/src/tests/authentication/account.test.ts
@@ -1,5 +1,5 @@
 import { Page } from "puppeteer";
-import request from "supertest";
+import request, { Test } from "supertest";
 
 import { AUTHENTICATION_HOST } from "../../constants/hosts";
 import newPage from "../../utilities/newPage";
@@ -15,17 +15,47 @@ describe(`${AUTHENTICATION_HOST}/account`, () => {
 
   describe("GET", () => {
     describe("without authentication header", () => {
+      let withoutAuthentication: Test;
+
+      beforeEach(() => {
+        withoutAuthentication = request(url)
+          .get("/");
+      });
+
       it("returns status code 200", done => {
-        request(url)
-          .get("/")
+        withoutAuthentication
           .expect(200)
           .end(done);
       });
 
       it("returns null account ID", done => {
-        request(url)
+        withoutAuthentication
+          .end(async (error, response) => {
+            expect(response.body).toEqual({
+              id: null,
+            });
+            done();
+          });
+      });
+    });
+
+    describe("with invalid authentication header", () => {
+      let withInvalidAuthentication: Test;
+
+      beforeEach(() => {
+        withInvalidAuthentication = request(url)
           .get("/")
+          .set("Authorization", "Bearer invalid_token");
+      });
+
+      it("returns status code 200", done => {
+        withInvalidAuthentication
           .expect(200)
+          .end(done);
+      });
+
+      it("returns null account ID", done => {
+        withInvalidAuthentication
           .end(async (error, response) => {
             expect(response.body).toEqual({
               id: null,
