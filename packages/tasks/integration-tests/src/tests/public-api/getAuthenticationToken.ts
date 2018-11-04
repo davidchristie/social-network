64962e9d683c9a04ba5a3ec19d2c9640b4c0d27d
David Christie <davidchristie.nz@gmail.com>
2018-11-04 13:18:56 +1300

    Add integration tests for public API

---
+++ packages/tasks/integration-tests/src/tests/public-api/getAuthenticationToken.ts
@@ -0,0 +1,29 @@
+import request from "supertest";
+
+import { AUTHENTICATION_HOST } from "../../constants/hosts";
+
+interface Arguments {
+  email: string;
+  password: string;
+}
+
+export default async function getAuthenticationToken (
+  { email, password }: Arguments
+) {
+  return new Promise(resolve => {
+    const url = `${AUTHENTICATION_HOST}/login`;
+    request(url)
+      .post("/")
+      .send({
+        email,
+        password,
+      })
+      .expect(200)
+      .end((error, response) => {
+        const { token } = response.body;
+        expect(error).toBeNull();
+        expect(token).toBeDefined();
+        resolve(token);
+      });
+  });
+}
---
+++ packages/tasks/integration-tests/src/tests/public-api/queries/account.test.ts
@@ -0,0 +1,60 @@
+import { ApolloClient } from "apollo-boost";
+import gql from "graphql-tag";
+
+import { PUBLIC_API_HOST } from "../../../constants/hosts";
+import { USER_1_EMAIL, USER_1_PASSWORD } from "../../../constants/login";
+import createPublicApiClient from "../createPublicApiClient";
+import getAuthenticationToken from "../getAuthenticationToken";
+
+const withIdField = gql`
+query {
+  account {
+    id
+  }
+}
+`;
+
+describe(PUBLIC_API_HOST, () => {
+  describe("account query", () => {
+    let client: ApolloClient<{}>;
+
+    describe("without authentication", () => {
+      beforeEach(() => {
+        client = createPublicApiClient();
+      });
+
+      it("returns null account", async () => {
+        const { data } = await client.query<any>({
+          query: withIdField,
+        });
+        expect(data!.account).toBeNull();
+      });
+    });
+
+    describe("with authentication", () => {
+      let token: string;
+
+      beforeAll(async () => {
+        token = await getAuthenticationToken({
+          email: USER_1_EMAIL,
+          password: USER_1_PASSWORD,
+        });
+      });
+
+      beforeEach(() => {
+        client = createPublicApiClient({
+          token,
+        });
+      });
+
+      describe("with ID field", () => {
+        it("matches snapshot", async () => {
+          const { data } = await client.query({
+            query: withIdField,
+          });
+          expect(data).toMatchSnapshot();
+        });
+      });
+    });
+  });
+});
