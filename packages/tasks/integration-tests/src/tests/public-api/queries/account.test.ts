42d36cbaf22e25ed33990691867cf6b018b0dc50
David Christie <davidchristie.nz@gmail.com>
2018-11-04 16:01:41 +1300

    Don't include host in test names

--- packages/tasks/integration-tests/src/tests/public-api/queries/account.test.ts
+++ packages/tasks/integration-tests/src/tests/public-api/queries/account.test.ts
@@ -65,46 +65,44 @@ const withAllPostFields = gql`
   }
 `;
 
-describe(PUBLIC_API_HOST, () => {
-  describe("account query", () => {
-    let client: ApolloClient<{}>;
+describe("account query", () => {
+  let client: ApolloClient<{}>;
 
-    describe("without authentication", () => {
-      beforeEach(() => {
-        client = createPublicApiClient();
+  describe("without authentication", () => {
+    beforeEach(() => {
+      client = createPublicApiClient();
+    });
+
+    it("returns null account", async () => {
+      const { data } = await client.query<any>({
+        query: withIdField,
       });
+      expect(data!.account).toBeNull();
+    });
+  });
 
-      it("returns null account", async () => {
-        const { data } = await client.query<any>({
-          query: withIdField,
-        });
-        expect(data!.account).toBeNull();
+  describe("with authentication", () => {
+    let token: string;
+
+    beforeAll(async () => {
+      token = await getAuthenticationToken({
+        email: USER_1_EMAIL,
+        password: USER_1_PASSWORD,
       });
     });
 
-    describe("with authentication", () => {
-      let token: string;
-
-      beforeAll(async () => {
-        token = await getAuthenticationToken({
-          email: USER_1_EMAIL,
-          password: USER_1_PASSWORD,
-        });
+    beforeEach(() => {
+      client = createPublicApiClient({
+        token,
       });
-
-      beforeEach(() => {
-        client = createPublicApiClient({
-          token,
-        });
-      });
-
-      describeQuery("with ID field", withIdField, () => client);
-
-      describeQuery("with all account fields", withAllAccountFields, () => client);
-
-      describeQuery("with all profile fields", withAllProfileFields, () => client);
-
-      describeQuery("with all post fields", withAllPostFields, () => client);
     });
+
+    describeQuery("with ID field", withIdField, () => client);
+
+    describeQuery("with all account fields", withAllAccountFields, () => client);
+
+    describeQuery("with all profile fields", withAllProfileFields, () => client);
+
+    describeQuery("with all post fields", withAllPostFields, () => client);
   });
 });
