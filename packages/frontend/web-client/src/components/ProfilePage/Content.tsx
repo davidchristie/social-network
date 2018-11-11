02954e21dd5dd5c6568d0e0d30c1a27876648a66
David Christie <davidchristie.nz@gmail.com>
2018-11-11 20:15:28 +1300

    Only show create post on own profile page

---
+++ packages/frontend/web-client/src/components/ProfilePage/Content.tsx
@@ -0,0 +1,42 @@
+import { Avatar, Container, Section } from "design-system";
+import React from "react";
+import CreatePostForm from "../CreatePostForm";
+import ProfilePosts from "../ProfilePosts";
+
+export interface Props {
+  account: {
+    profile: {
+      id: string;
+    };
+  } | null;
+  profile: {
+    avatar: {
+      url: string;
+    } | null;
+    id: string;
+    name: string;
+  };
+}
+
+const Content: React.SFC<Props> = ({ account, profile }) => {
+  const isOwnProfile = account && account.profile.id === profile.id;
+  return (
+    <Container>
+      <Section>
+        <Avatar
+          image={profile.avatar ? profile.avatar.url : undefined}
+          size="large"
+        />
+        <h1>{profile.name}</h1>
+      </Section>
+      <Section>
+        {isOwnProfile && (
+          <CreatePostForm />
+        )}
+        <ProfilePosts profileId={profile.id} />
+      </Section>
+    </Container>
+  );
+};
+
+export default Content;
--- packages/frontend/web-client/src/components/ProfilePage/index.tsx
+++ packages/frontend/web-client/src/components/ProfilePage/index.tsx
@@ -1,62 +1,7 @@
-import {
-  Alert,
-  Avatar,
-  Container,
-  Loading,
-  Section
-} from "design-system";
 import React from "react";
-import { Query } from "react-apollo";
+import Connected from "./Connected";
+import Content from "./Content";
 
-import ProfileQuery, {
-  ProfileData,
-  ProfileVariables
-} from "../../queries/Profile";
-import CreatePostForm from "../CreatePostForm";
-import ProfilePosts from "../ProfilePosts";
+const ProfilePage: React.SFC<{}> = () => <Connected content={Content} />;
 
-interface Props {
-  match: {
-    params: {
-      id: string;
-    }
-  };
-}
-
-export default class ProfilePage extends React.Component<Props> {
-  public render () {
-    return (
-      <Query<ProfileData, ProfileVariables>
-        variables={{
-          id: this.props.match.params.id,
-        }}
-        query={ProfileQuery}
-      >
-        {({ data, error, loading }) => {
-          if (loading) {
-            return <Loading />;
-          }
-          if (error) {
-            return <Alert>{error.message}</Alert>;
-          }
-          const { profile } = data!;
-          return (
-            <Container>
-              <Section>
-                <Avatar
-                  image={profile.avatar ? profile.avatar.url : undefined}
-                  size="large"
-                />
-                <h1>{profile.name}</h1>
-              </Section>
-              <Section>
-                <CreatePostForm />
-                <ProfilePosts profileId={profile.id} />
-              </Section>
-            </Container>
-          );
-        }}
-      </Query>
-    );
-  }
-}
+export default ProfilePage;
