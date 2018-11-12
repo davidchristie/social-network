dc3c356ae1bce9d063da2b9872931e55b9b07995
David Christie <davidchristie.nz@gmail.com>
2018-11-12 19:51:10 +1300

    Fix types in authentication service

--- packages/backend/authentication/src/utilities/getToken.ts
+++ packages/backend/authentication/src/utilities/getToken.ts
@@ -1,8 +1,8 @@
-import { AccountNode } from "data-model";
+import { Account } from "data-model";
 import { sign } from "jsonwebtoken";
 
 import getJwtSecret from "./getJwtSecret";
 
-export default function getToken (account: AccountNode) {
+export default function getToken (account: Account) {
   return sign({ accountId: account.id }, getJwtSecret());
 }
