2df05fa80d8f3f31471125ab5d9eb2e987ff4863
David Christie <davidchristie.nz@gmail.com>
2018-11-19 19:31:33 +1300

    Unit test update profile form component

---
+++ packages/frontend/web-client/src/components/UpdateProfileForm/index.test.ts
@@ -0,0 +1,6 @@
+import { itRendersWithoutCrashing } from "test-utilities/react";
+import UpdateProfileForm from ".";
+
+describe("UpdateProfileForm component", () => {
+  itRendersWithoutCrashing(UpdateProfileForm);
+});
