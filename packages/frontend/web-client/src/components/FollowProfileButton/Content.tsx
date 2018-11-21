6c8cf1fcd7d0596947a447a7565bd1e3d44a0da0
David Christie <davidchristie.nz@gmail.com>
2018-11-21 19:54:59 +1300

    Perform mutations when button is clicked

--- packages/frontend/web-client/src/components/FollowProfileButton/Content.tsx
+++ packages/frontend/web-client/src/components/FollowProfileButton/Content.tsx
@@ -1,5 +1,8 @@
 import { Button } from "design-system";
 import React from "react";
+import { MutationFn } from "react-apollo";
+import { FollowProfileData, FollowProfileVariables } from "../../mutations/FollowProfile";
+import { UnfollowProfileData, UnfollowProfileVariables } from "../../mutations/UnfollowProfile";
 
 export interface Props {
   account: {
@@ -9,16 +12,23 @@ export interface Props {
       }>;
     };
   };
+  followProfile: MutationFn<FollowProfileData, FollowProfileVariables>;
   profileId: string;
+  unfollowProfile: MutationFn<UnfollowProfileData, UnfollowProfileVariables>;
 }
 
-const Content: React.StatelessComponent<Props> = ({ account, profileId }) => {
+const Content: React.StatelessComponent<Props> = ({
+  account,
+  followProfile,
+  profileId,
+  unfollowProfile,
+}) => {
   const isFollowing = account.profile.following.some(followed => {
     return followed.id === profileId;
   });
   return isFollowing
-    ? <Button>Unfollow</Button>
-    : <Button>Follow</Button>;
+    ? <Button onClick={() => unfollowProfile()}>Unfollow</Button>
+    : <Button onClick={() => followProfile()}>Follow</Button>;
 };
 
 export default Content;
