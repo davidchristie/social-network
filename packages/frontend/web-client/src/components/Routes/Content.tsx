9ce152a0836e5caba399f5e492dec3bbcd82493b
David Christie <davidchristie.nz@gmail.com>
2018-11-25 19:18:39 +1300

    Refactor routes data

--- packages/frontend/web-client/src/components/Routes/Content.tsx
+++ packages/frontend/web-client/src/components/Routes/Content.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Page from "../Page";
 
-interface Props {
+export interface Props {
   authenticated: boolean;
 }
 
--- packages/frontend/web-client/src/components/Routes/index.tsx
+++ packages/frontend/web-client/src/components/Routes/index.tsx
@@ -1,23 +1,7 @@
-import { Alert, Loading } from "design-system";
 import React from "react";
-import AccountQuery from "../AccountQuery";
 import Content from "./Content";
+import Data from "./Data";
 
-export default class Routes extends React.Component {
-  public render () {
-    return (
-      <AccountQuery>
-        {({ data, error, loading }) => {
-          if (error) {
-            return <Alert>{error.message}</Alert>;
-          }
-          if (!data || loading) {
-            return <Loading />;
-          }
-          const authenticated = Boolean(data.account);
-          return <Content authenticated={authenticated} />;
-        }}
-      </AccountQuery>
-    );
-  }
-}
+const Routes: React.StatelessComponent<{}> = () => <Data content={Content} />;
+
+export default Routes;
