cbdc28f8b629317c5a234dce43d462d1e24d7703
David Christie <davidchristie.nz@gmail.com>
2018-12-08 21:45:38 +1300

    Move update account forms into domain model

--- packages/frontend/domain-model/src/index.ts
+++ packages/frontend/domain-model/src/index.ts
@@ -1,2 +1,5 @@
 export { default as LoginForm } from "./components/LoginForm";
 export { default as SignupForm } from "./components/SignupForm";
+export { default as UpdateAccountForm } from "./components/UpdateAccountForm";
+export { default as UpdatePasswordForm } from "./components/UpdatePasswordForm";
+export { default as UpdateProfileForm } from "./components/UpdateProfileForm";
