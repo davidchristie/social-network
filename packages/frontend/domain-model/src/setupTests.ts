af52502d7fd384255f9e06c2dd2aa091ac54c225
David Christie <davidchristie.nz@gmail.com>
2018-12-10 22:09:10 +1300

    Mock global fetch

--- packages/frontend/domain-model/src/setupTests.ts
+++ packages/frontend/domain-model/src/setupTests.ts
@@ -4,3 +4,6 @@ import Adapter from "enzyme-adapter-react-16";
 Enzyme.configure({
   adapter: new Adapter(),
 });
+
+// Mock global fetch
+window.fetch = (() => Promise.resolve({} as Response)) as GlobalFetch["fetch"];
