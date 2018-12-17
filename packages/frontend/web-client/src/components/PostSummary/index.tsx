970639da10e9a76546b9a1f36024d7eb9683d5ed
David Christie <davidchristie.nz@gmail.com>
2018-12-17 20:58:32 +1300

    Move relative date component into design system

--- packages/frontend/web-client/src/components/PostSummary/index.tsx
+++ packages/frontend/web-client/src/components/PostSummary/index.tsx
@@ -1,10 +1,8 @@
-import { Avatar } from "design-system";
+import { Avatar, RelativeDate } from "design-system";
 import React from "react";
 import { Link } from "react-router-dom";
-
 import { PostSummaryData } from "../../fragments/PostSummary";
 import PostMenu from "../PostMenu";
-import RelativeDate from "../RelativeDate";
 import "./index.css";
 
 interface Props {
