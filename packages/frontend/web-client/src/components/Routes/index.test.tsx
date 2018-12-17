77df13c219f39b8be387fd83027d31b35cda1ac0
David Christie <davidchristie.nz@gmail.com>
2018-12-17 20:10:37 +1300

    Refactor routes component

--- packages/frontend/web-client/src/components/Routes/index.test.tsx
+++ packages/frontend/web-client/src/components/Routes/index.test.tsx
@@ -1,6 +1,24 @@
-import { itRendersWithoutCrashing } from "test-utilities/react";
+import { shallow, ShallowWrapper } from "enzyme";
+import React from "react";
+import { Route } from "react-router-dom";
 import Routes from ".";
 
 describe("Routes component", () => {
-  itRendersWithoutCrashing(Routes);
+  let wrapper: ShallowWrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Routes />);
+  });
+
+  it("matches snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe("each route", () => {
+    it("renders without crashing", () => {
+      wrapper.find(Route).forEach(route => {
+        route.props().render();
+      });
+    });
+  });
 });
