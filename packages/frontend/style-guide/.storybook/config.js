ad8c4780ab0aad294e400b14d36dabc2999f5642
David Christie <davidchristie.nz@gmail.com>
2018-12-23 13:17:31 +1300

    Update style guide theme

--- packages/frontend/style-guide/.storybook/config.js
+++ packages/frontend/style-guide/.storybook/config.js
@@ -1,11 +1,17 @@
-import { configure } from '@storybook/react';
+import { withOptions } from "@storybook/addon-options";
+import { addDecorator, configure } from "@storybook/react";
 
-const requireDesignSystem = require.context('../../design-system/src/components', true, /\.stories\.tsx?$/)
-const requireDomainModel = require.context('../../domain-model/src/components', true, /\.stories\.tsx?$/)
+addDecorator(withOptions({
+  name: "Style Guide",
+  url: "https://github.com/davidchristie/social-network",
+}));
+
+const requireDesignSystem = require.context("../../design-system/src/components", true, /\.stories\.tsx?$/);
+const requireDomainModel = require.context("../../domain-model/src/components", true, /\.stories\.tsx?$/);
 
 function loadStories () {
-  requireDesignSystem.keys().forEach((filename) => requireDesignSystem(filename))
-  requireDomainModel.keys().forEach((filename) => requireDomainModel(filename))
+  requireDesignSystem.keys().forEach((filename) => requireDesignSystem(filename));
+  requireDomainModel.keys().forEach((filename) => requireDomainModel(filename));
 }
 
 configure(loadStories, module);
