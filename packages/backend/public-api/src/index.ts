e5c8643bb250541c039eb7cd1bda861b8d3205b6
David Christie <davidchristie.nz@gmail.com>
2018-12-25 10:09:36 +1300

    Set limit on GraphQL query complexity

--- packages/backend/public-api/src/index.ts
+++ packages/backend/public-api/src/index.ts
@@ -1,5 +1,20 @@
+import costAnalysis from "graphql-cost-analysis";
+import { Options } from "graphql-yoga";
 import server from "./server";
 
-server.start(({ port }) => {
+const options: Options = {
+  validationRules: request => [
+    costAnalysis({
+      defaultCost: 1,
+      maximumCost: 50,
+      onComplete: cost => {
+        console.log(`Cost analysis score: ${cost}`);
+      },
+      variables: request.query.variables,
+    }),
+  ],
+};
+
+server.start(options, ({ port }) => {
   console.log(`Running on http://localhost:${port}`);
 });
