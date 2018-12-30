c87f08dc8cf817172084e142393938efca508375
David Christie <davidchristie.nz@gmail.com>
2018-12-30 13:32:48 +1300

    Refactor text area component

--- packages/frontend/design-system/src/components/TextArea/index.stories.ts
+++ packages/frontend/design-system/src/components/TextArea/index.stories.ts
@@ -1,9 +1,11 @@
 import { action } from "@storybook/addon-actions";
-
 import TextArea from ".";
 import addComponentStory from "../../utilities/storybook/addComponentStory";
 
 addComponentStory(TextArea, {
+  id: "text-area-id",
+  label: "TextArea",
+  name: "text-area",
   onBlur: action("onBlur"),
   onChange: action("onChange"),
   onClick: action("onClick"),
