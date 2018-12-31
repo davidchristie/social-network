d7268f5115baf1d760aaf709780bbcbbee566d79
David Christie <davidchristie.nz@gmail.com>
2018-12-31 20:46:33 +1300

    Refactor login form data

--- packages/frontend/domain-model/src/components/LoginForm/index.tsx
+++ packages/frontend/domain-model/src/components/LoginForm/index.tsx
@@ -1,30 +1,11 @@
 import React from "react";
-import { Mutation } from "react-apollo";
-import { AUTHENTICATION_TOKEN } from "../../constants";
-import { Login, LoginVariables } from "../../generated/types";
-import LOGIN_MUTATION from "../../mutations/Login";
-import AccountQuery from "../../queries/Account";
 import Content from "./Content";
+import Data from "./Data";
 
-export default class LoginForm extends React.Component {
-  public render () {
-    return (
-      <Mutation<Login, LoginVariables>
-        mutation={LOGIN_MUTATION}
-        refetchQueries={[
-          {
-            query: AccountQuery,
-          },
-        ]}
-        update={({ }, { data }) => {
-          if (data) {
-            const { token } = data.login;
-            window.localStorage.setItem(AUTHENTICATION_TOKEN, token);
-          }
-        }}
-      >
-        {(login) => <Content login={login} />}
-      </Mutation>
-    );
-  }
-}
+const LoginForm: React.StatelessComponent = () => {
+  return (
+    <Data content={Content} />
+  );
+};
+
+export default LoginForm;
