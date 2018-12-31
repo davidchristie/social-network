5e72e8166bdf9d0c2bd4dc6ec0c31c6fe65b7685
David Christie <davidchristie.nz@gmail.com>
2018-12-31 19:34:13 +1300

    Refactor login form content

--- packages/frontend/domain-model/src/components/LoginForm/index.tsx
+++ packages/frontend/domain-model/src/components/LoginForm/index.tsx
@@ -1,37 +1,16 @@
-import { Alert, Button, Input, Section } from "design-system";
 import React from "react";
 import { Mutation } from "react-apollo";
-import { Link } from "react-router-dom";
 import { AUTHENTICATION_TOKEN } from "../../constants";
 import { Login, LoginVariables } from "../../generated/types";
 import LOGIN_MUTATION from "../../mutations/Login";
 import AccountQuery from "../../queries/Account";
+import Content from "./Content";
 
-export interface State {
-  email: string;
-  error: string | null;
-  password: string;
-}
-
-export default class LoginForm extends React.Component<{}, State> {
-  public state: State = {
-    email: "",
-    error: null,
-    password: "",
-  };
-
+export default class LoginForm extends React.Component {
   public render () {
-    const { email, password } = this.state;
     return (
       <Mutation<Login, LoginVariables>
         mutation={LOGIN_MUTATION}
-        onError={() => {
-          this.setState({
-            email: "",
-            error: "Incorrect email or password.",
-            password: "",
-          });
-        }}
         refetchQueries={[
           {
             query: AccountQuery,
@@ -43,63 +22,9 @@ export default class LoginForm extends React.Component<{}, State> {
             window.localStorage.setItem(AUTHENTICATION_TOKEN, token);
           }
         }}
-        variables={{
-          email,
-          password,
-        }}
       >
-        {(login) => (
-          <Section className="LoginForm">
-            <form
-              onSubmit={async (event: React.FormEvent) => {
-                event.preventDefault();
-                login();
-              }}
-            >
-              <h2>Login</h2>
-              Don't have an account? <Link to="/signup">Click here to sign up.</Link>
-              <hr />
-              {
-                this.state.error && (
-                  <Alert>{this.state.error}</Alert>
-                )
-              }
-              <Input
-                autoFocus={true}
-                id="login-email"
-                label="Email Address"
-                name="email"
-                onChange={this.emailChanged}
-                required={true}
-                type="email"
-                value={this.state.email}
-              />
-              <Input
-                id="login-password"
-                label="Password"
-                name="password"
-                onChange={this.passwordChanged}
-                required={true}
-                type="password"
-                value={this.state.password}
-              />
-              <Button type="submit">Submit</Button>
-            </form>
-          </Section>
-        )}
+        {(login) => <Content login={login} />}
       </Mutation>
     );
-  }
-
-  private emailChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      email: event.target.value,
-    });
-  }
-
-  private passwordChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      password: event.target.value,
-    });
   }
 }
